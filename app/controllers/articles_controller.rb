98c31ff53ef7fae863ed9481f2f52da608b12caf
nmehanma <nmehanma@gmail.com>
2020-01-05 18:01:06 -0500

    working on editing and submitting

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,32 @@
 class ArticlesController < ApplicationController
+  
+ 
+
+  def index
+    @article = Article.all
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
   def new
     @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
+
     if @article.save
       flash[:notice] = "Article was sucessfully created"
       redirect_to article_path(@article)
@@ -13,10 +35,9 @@ class ArticlesController < ApplicationController
    end
   end 
 
-   def show
+  def show
     @article = Article.find(params[:id])
-
-    end
+  end
 
 
   private
