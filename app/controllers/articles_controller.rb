25b366011b6803a85a574bb67b9070cd4eb8db7a
nmehanma <nmehanma@gmail.com>
2020-01-04 22:42:25 -0500

    first attempt at making paths connect to each other

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,12 +4,19 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to_articles_show(@article)
-    
-  end
+    if @article.save
+      flash[:notice] = "Article was sucessfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+   end
+  end 
+
+   def show
+    @article = Article.find(params[:id])
+
+    end
 
 
   private
