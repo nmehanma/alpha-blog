115827a1306337561415cfc91567fb18a115f08a
nmehanma <nmehanma@gmail.com>
2020-01-27 22:29:49 -0500

    add pagination to views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only:[:edit, :update, :show, :destroy]
   
   def index
-    @categories = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 2)
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 2)
   end
   
   
@@ -36,7 +36,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 2)
   end
+
+
 
 
   private
