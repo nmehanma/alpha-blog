608aeaf39499f3ae87acfd32c4164a6e7270b43e
nmehanma <nmehanma@gmail.com>
2020-02-03 00:13:01 -0500

    add admin requirement to create categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -36,7 +36,7 @@ class CategoriesController < ApplicationController
 
   def require_admin
     if !logged_in? || (logged_in? and !current_user.admin?)
-      flash[:danger] = "Only admins can perform this that action"
+      flash[:danger] = "Only admins can perform this action"
       redirect_to categories_path
     end
   end
