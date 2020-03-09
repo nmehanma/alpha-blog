d80fb656c890851ef0f150d78e86170f57b84ceb
nmehanma <nmehanma@gmail.com>
2020-03-08 20:24:17 -0400

    modified sessinos_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,30 +1,68 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
 
   def new
-
   end
 
-
   def create
-    user= User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:success] = "You have successfully logged in!"
-      redirect_to users_path(user)
-
+      flash[:success] = "You have successfully logged in"
+      redirect_to root_path
     else
       flash.now[:danger] = "There was something wrong with your login information"
       render 'new'
     end
-
   end
 
   def destroy
-
-    session[:user_id]= nil
-    flash[:success]= "You have logged out!"
-    redirect_to root_path
-
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out"
+    redirect_to login_path
   end
 
+  private
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in"
+      redirect_to root_path
+    end
+  end
 end
+
+
+
+
+
+# def new
+
+# end
+
+
+# def create
+#   user= User.find_by(email: params[:session][:email].downcase)
+#   if user && user.authenticate(params[:session][:password])
+#     session[:user_id] = user.id
+#     flash[:success] = "You have successfully logged in!"
+#     redirect_to users_path(user)
+
+#   else
+#     flash.now[:danger] = "There was something wrong with your login information"
+#     render 'new'
+#   end
+
+# end
+
+# def destroy
+
+#   session[:user_id]= nil
+#   flash[:success]= "You have logged out!"
+#   redirect_to root_path
+
+# end
+
+# end
+
+# Learn more or give us feedback
